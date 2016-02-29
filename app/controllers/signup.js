f477e2dd48380255fee753fd2936fc83dd5a9740
Ryan Forbes <ryfo18@gmail.com>
2016-02-29 07:34:11 -0600

    Working to get signup form working

--- app/controllers/signup.js
+++ app/controllers/signup.js
@@ -4,7 +4,17 @@ import EmberValidations from 'ember-validations';
 export default Ember.Controller.extend(EmberValidations, {
   validations: {
     email: {
-      format: { with: /^(\w+@\w+\.\w+)/, allowBlank: true, message: "Enter a valid e-mail address" }
+      format: { with: /^(\w+@\w+\.\w+)/, allowBlank: false, message: "Enter a valid e-mail address" }
+    }
+  },
+  actions: {
+    createUser: function() {
+      var data = this.getProperties('email')
+      $.post('http://127.0.0.1:8000/api/signups/', data, null, 'json').then(
+        function(response) {
+          this.reset();
+        }
+      )
     }
   }
 });
--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route("signup",{ path: "/signup" });
+  this.route("signup");
 });
 
 export default Router;
